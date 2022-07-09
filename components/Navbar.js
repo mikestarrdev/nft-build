9df5818cf7c4680adcc8e600e7201a04fce913ac
Mike Starr <imhs@protonmail.com>
2022-07-09 16:13:45 -0400

    Added web3modal to useEffect hook, now it works. Need to refactor code still

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,22 @@
-function Navbar({ connect, userAccount, disconnect }) {
+function Navbar({ connect, account, disconnect }) {
+  console.log("account:", account);
   return (
-    <nav className="flex justify-center bg-sky-300 font-medium text-xl">
+    <nav className="flex justify-center bg-sky-300 text-lg">
       <ul className="flex flex-row items-center align-center">
         <li className="m-3">Home</li>
         <li className="m-3">My Page</li>
         <li className="m-3">Create NFT</li>
         <li className="m-3">
-          {!userAccount ? (
+          {!account || account === null || account === "" ? (
             <button
-              onClick={null}
+              onClick={connect}
               className=" basis-2/5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-3"
             >
               Connect Wallet
             </button>
           ) : (
             <button
-              onClick={null}
+              onClick={disconnect}
               className=" basis-2/5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-3"
             >
               Disconnect Wallet
